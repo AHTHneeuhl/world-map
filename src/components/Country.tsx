4bdd70d9220bf57fb5b6212cc6f9678d6db6ac48
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-06-17 17:05:58 +0530

    styles: update country and map styles (#2)

--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,5 +1,6 @@
 import useCountries from "@/hooks/useCountries";
 import { Text, Spinner } from "@/components/common";
+import { numberWithCammas } from "@/utils/helpers";
 
 const Country: React.FC = () => {
   const { isLoading, country } = useCountries("India");
@@ -7,8 +8,8 @@ const Country: React.FC = () => {
   if (isLoading) return <Spinner />;
 
   return (
-    <div className="border border-neutral-100 w-full h-[80vh] rounded-2xl p-4 hover:shadow cursor-pointer transition">
-      <div className="flex flex-col items-center justify-center gap-3">
+    <div className="border border-neutral-100 w-96 h-[80vh] rounded-2xl px-4 py-6 hover:shadow cursor-pointer transition">
+      <div className="flex flex-col items-center justify-center gap-3 mb-5">
         <img
           src={country?.flags.png}
           alt={country?.flags.alt}
@@ -18,26 +19,32 @@ const Country: React.FC = () => {
           {country?.officialName}
         </h3>
       </div>
-      <div className="flex flex-row items-center  ">
+      <div className="flex flex-row items-center justify-between">
         <Text label="Capital" />
         {country?.capital.map((item: string) => (
           <Text key={item} content={item} />
         ))}
       </div>
-      <div className="flex flex-row items-center  ">
-        <Text label="Currency" content={country?.currencies?.INR?.name} />
+      <div className="flex flex-row items-center justify-between">
+        <Text label="Currency" />
       </div>
-      <Text label="Population" content={country?.population} />
-      <Text label="Area" content={country?.area} />
+      <Text
+        label="Population"
+        content={country?.population && numberWithCammas(country.population)}
+      />
+      <Text
+        label="Area"
+        content={country?.area && numberWithCammas(country.area)}
+      />
       <Text label="Region" content={country?.region} />
       <Text label="Subregion" content={country?.subregion} />
-      <div className="flex flex-row items-center  ">
+      <div className="flex flex-row items-center justify-between">
         <Text label="Timezones" />
         {country?.timezones.map((timezone: string) => (
           <Text key={timezone} content={timezone} />
         ))}
       </div>
-      <div className="flex flex-row items-center  ">
+      <div className="flex flex-row items-center justify-between">
         <Text label="Borders" />
         <div className="flex flex-row gap-2">
           {country?.borders.map((border: string) => (
@@ -50,7 +57,7 @@ const Country: React.FC = () => {
           ))}
         </div>
       </div>
-      <div className="flex flex-row gap-3 items-center my-2">
+      <div className="flex flex-row gap-3 items-center my-2 justify-between">
         <Text label="Maps" />
         <button
           onClick={() => window.open(country?.maps.googleMaps, "_blank")}
